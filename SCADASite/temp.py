6f4259f8bdef2bc92ef6e5b47b6dfb67d88e7f7e
bjh7242 <bjh7242@rit.edu>
2015-12-22 17:20:30 -0500

    added for loop to update temps in all team files

--- SCADASite/temp.py
+++ SCADASite/temp.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 import random
+import time
 
-f = open('t1-temp.txt','w')
-temp = random.randint(68,72)
-print str(temp)
-f.write(str(temp))
-f.close()
+numteams = 10
+
+while True:
+    for i in range(1,numteams+1):
+        teamfile = "t" + str(i) + "-temp.txt"
+        print teamfile
+        f = open(teamfile,'w')
+        temp = random.randint(68,72)
+        print str(temp)
+        f.write(str(temp))
+        f.close()
+    time.sleep(1)
