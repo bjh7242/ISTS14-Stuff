618b748fc43b5e1b3303a54f74014e38610c0e43
bjh7242 <bjh7242@rit.edu>
2015-12-22 18:37:20 -0500

    working on changing background color

--- SCADASite/js/get-temp.js
+++ SCADASite/js/get-temp.js
@@ -8,6 +8,13 @@ function loadDoc(url, teamID) {
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       document.getElementById(teamID).innerHTML = xhttp.responseText + "&deg;F";
+      var div = document.getElementById("t5box");
+      if (xhttp.responseText > 70) {
+        div.style.backgroundColor="red";
+      }
+      else {
+        div.style.backgroundColor="green";
+      }
     }
   };
   xhttp.open("GET", url, true);
