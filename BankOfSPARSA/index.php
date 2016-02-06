554b7de2c8006fa9c216eb852e08fdbdc6ce1113
bjh7242 <bjh7242@rit.edu>
2016-02-06 18:42:01 -0500

    modified login form

--- BankOfSPARSA/index.php
+++ BankOfSPARSA/index.php
@@ -4,9 +4,11 @@
 </head>
 <body>
   <h1>Bank of SPARSA</h1>
-  <?php
-  echo hello;
-  phpinfo()
-  ?>
+  <h2>Login</h2>
+  <form name="login" action="session.php" method="post">
+    Username: <input type="text" name="username" value="Username"><br />
+    Password: <input type="password" name="password" value="password"><br />
+    <input type="submit" name="submit" value="Submit">
+  </form>
 </body>
 </html>
