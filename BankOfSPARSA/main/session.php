49b1a9656c89031392a7f29fc4f306a0e5a5b9d8
bjh7242 <bjh7242@rit.edu>
2016-02-11 21:46:04 -0500

    added mysql prepared statement for checking username/pass

--- BankOfSPARSA/main/session.php
+++ BankOfSPARSA/main/session.php
@@ -6,43 +6,61 @@
   include("$root/includes/functions.inc.php");
 
   // connect to the DB
-  $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-  $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+  //$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+  //$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+  $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
   //print "USERNAME IS " . $_POST['username'];
-  if(!empty($_POST['username'])) {
+  if(!empty($_POST['username']) and !empty($_POST['password'])) {
     //echo "query = mysql_query(\"SELECT * FROM login where username = '$_POST[username]' AND password = '$_POST[password]'\") or die(mysql_error());";
-    $query = mysql_query("SELECT * FROM login where username = '$_POST[username]' AND password = '$_POST[password]'") or die(mysql_error());
-  	$row = mysql_fetch_array($query) or die(mysql_error());
-    if(isset($row['username']) && isset($row['password']) && !empty($row['username']) && !empty($row['password'])) {
-  		$_SESSION['username'] = $row['username'];
-      $_SESSION['name'] = $row['name'];
-      $_SESSION['emailAddr'] = $row['emailAddr'];
-      $_SESSION['userID'] = $row['userID'];
-  		//echo $row['role'] . "\r\n";
-      if($row['role'] == 'admin') {
-        $_SESSION['role'] = 'admin';
-        //echo "SUCCESSFULLY LOGGED IN TO ADMIN PROFILE PAGE...\n";
-        //print_r($_SESSION);
-        redirect("../admin/panel.php");
+    //$query = mysql_query("SELECT * FROM login where username = '$_POST[username]' AND password = '$_POST[password]'") or die(mysql_error());
+  	//$row = mysql_fetch_array($query);
+
+    // prepare select statement
+    if (!($stmt = $mysqli->prepare("SELECT userID,name,username,password,role,emailAddr FROM login WHERE username = ? AND password = ?"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    // username and password are both strings (first param in bind_param)
+    if (!$stmt->bind_param("ss", $_POST['username'], $_POST['password'])) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    // bind the results of the query to each field
+    $stmt->bind_result($userID,$name,$username,$password,$role,$emailAddr);
+
+    // store the results of the query in order to count the num of results
+    $result = $stmt->store_result();
+    if ($stmt->num_rows() === 1) {
+      while ($stmt->fetch()) {
+        //store the values for the last matched user
+        $_SESSION['userID'] = $userID;
+        $_SESSION['name'] = $name;
+        $_SESSION['username'] = $username;
+        $_SESSION['emailAddr'] = $emailAddr;
+        $_SESSION['role'] = $role;
       }
-      elseif($row['role'] == 'user') {
-        $_SESSION['role'] = 'user';
-        //echo "SUCCESSFULLY LOGGED IN TO USER PROFILE PAGE...\n";
-        redirect("../user/panel.php");
-      }
-  	}
-  	else {
-      print_r($_SESSION);
-  		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
-      //redirect('./index.php');
-  	}
+    }
+    else {
+      echo "incorrect username/password :(";
+      exit();
+    }
+
+    echo "variables: $userID $name $username $emailAddr $role";
+
+    if($role === 'admin') {
+      // if the role is an admin, redirect to the admin page
+      redirect("../admin/panel.php");
+    }
+    elseif($role=== 'user') {
+      // if the role is a user, redirect to user panel
+      redirect("../user/panel.php");
+    }
   }
 
-  /*
-  if(isset($_POST['submit']))
-  {
-    SignIn();
-  }
-  */
 ?>
