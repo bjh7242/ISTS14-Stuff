ee3439582b38e51cb35bc13daab475ca8fc4470c
bjh7242 <bjh7242@rit.edu>
2016-02-12 00:46:18 -0500

    added functions to pull account info for user

--- BankOfSPARSA/includes/functions.inc.php
+++ BankOfSPARSA/includes/functions.inc.php
@@ -1,9 +1,81 @@
 <?php
+include_once("config.inc.php");
 ini_set('display_errors', 'On');
 
-function redirect($page)
-{
+function redirect($page) {
   header('Location: ' . $page);
   exit();
 }
+
+function get_accountNum($username) {
+  $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+  // verify that the new user does not exist already
+  // prepare select statement
+  //select userID,accountNum,login.username from accounts join login using(userID) where username = ?
+  if (!($stmt = $mysqli->prepare("SELECT userID,accountNum,login.username FROM accounts JOIN login using(userID) WHERE username = ?"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+  }
+
+  // bind username and emailAddr params
+  if (!$stmt->bind_param("s", $username)) {
+    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+  }
+
+  if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+  }
+
+  // bind the results of the query to each field
+  $stmt->bind_result($userID,$acct,$user_name);
+
+  // make sure only 1 account exists
+  $result = $stmt->store_result();
+  if ($stmt->num_rows() === 1) {
+    while ($stmt->fetch()) {
+      $accountNum = $acct;
+    }
+  }
+
+  $stmt->close();
+  $mysqli->close();
+
+  return $accountNum;
+}
+
+function get_balance($accountNum) {
+  $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+  // verify that the new user does not exist already
+  // prepare select statement
+  if (!($stmt = $mysqli->prepare("SELECT balance FROM accounts where accountNum = ?"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+  }
+
+  // bind username and emailAddr params
+  if (!$stmt->bind_param("s", $accountNum)) {
+    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+  }
+
+  if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+  }
+
+  // bind the results of the query
+  $stmt->bind_result($balance);
+
+  // make sure only 1 account exists, set the balance to $bal and return that
+  $result = $stmt->store_result();
+  if ($stmt->num_rows() === 1) {
+    while ($stmt->fetch()) {
+      $bal = $balance;
+    }
+  }
+
+  $stmt->close();
+  $mysqli->close();
+
+  return round($bal,2);
+}
+
 ?>
--- BankOfSPARSA/user/panel.php
+++ BankOfSPARSA/user/panel.php
@@ -1,5 +1,7 @@
 <?php
   session_start();
+  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+  include("$root/includes/functions.inc.php");
 
 ?>
 <html>
@@ -7,8 +9,16 @@
   <title>Bank of SPARSA | User Panel</title>
 </header>
 <body>
-  <h1>Welcome <?php echo $_SESSION['name']; ?>!</h1>
+  <h1>Welcome <?php echo $_SESSION['name']; ?>! Your last login was TIME</h1>
   <h2>Welcome to the User Panel</h2>
+  Account Number: <?php echo get_accountNum("whatever"); ?><br>
+  Balance: <?php echo get_balance('4230532417'); ?>
+  <br>
   <ul>
-    <li><a href="/admin/user_mgmt.php">Check Balance</a></li>
+    <li><a href="/user/user_mgmt.php">Check Balance</a></li>
+    <li><a href="">Transfer Money</a></li>
+    <li><a href="">Transaction History</a></li>
+    <li><a href="">Schedule Transfer</a></li>
+    <li><a href="">Check Transfer Schedule</a></li>
+    <li><a href="">Change PIN</a></li>
   </ul>
