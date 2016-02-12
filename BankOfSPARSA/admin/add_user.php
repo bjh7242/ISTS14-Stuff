88e25484d1be52c691b15eac0a2fcd02b3661314
bjh7242 <bjh7242@rit.edu>
2016-02-11 23:12:21 -0500

    added prepare statements for user account creation

--- BankOfSPARSA/admin/add_user.php
+++ BankOfSPARSA/admin/add_user.php
@@ -5,62 +5,120 @@
   include_once("$root/includes/config.inc.php");
 
   if (isset($_POST['name']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['emailAddr']) && isset($_POST['role'])) {
-    $name = $_POST['name'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['emailAddr'];
-    $role = $_POST['role'];
-
     // connect to DB
     $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
     // might need to do error handling for db connection here?
 
     // verify that the new user does not exist already
-    $userCheck = $mysqli->query("SELECT * FROM login where username = '$username'");
-    $userExists = $userCheck->fetch_assoc();
-    if (isset($userExists)) {
-      echo "Username or email address is already registered.";
-      exit();
+    // prepare select statement
+    if (!($stmt = $mysqli->prepare("SELECT username FROM login where username = ? OR emailAddr = ?"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
 
-    // verify that the email addr of the new user does not exist already
-    $emailCheck = $mysqli->query("SELECT * FROM login where emailAddr = '$email'");
-    $emailExists = $userCheck->fetch_assoc();
-    if (isset($emailExists)) {
-      echo "Username or email address is already registered.";
-      exit();
+    // bind username and emailAddr params
+    if (!$stmt->bind_param("ss", $_POST['username'], $_POST['emailAddr'])) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
     }
 
-    // add the user to the DB
-    $mysqli->query("INSERT INTO login (name,username,password,emailAddr,role) values ('$name','$username','$password','$email','$role')");
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
 
-    // get user ID of new user
-    $result = $mysqli->query("SELECT * FROM login WHERE username = '$username'");
-    $row = $result->fetch_assoc();
-    $newUserID = $row['userID'];
+    // store the results of the query in order to count the num of results
+    $result = $stmt->store_result();
+    if ($stmt->num_rows() !== 0) {
+      echo "Username or Email Address Already exists.";
+      die();
+    }
+    $stmt->close();
+
+    // if username or email is not in the database, add the user to the DB
+    // prepare insert statement
+    if (!($stmt = $mysqli->prepare("INSERT INTO login (name,username,password,emailAddr,role) values (?, ?, ?, ?, ?)"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    // bind username and emailAddr params
+    if (!$stmt->bind_param("sssss", $_POST['name'], $_POST['username'], $_POST['password'], $_POST['emailAddr'], $_POST['role'])) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+    $stmt->close();
+
+    // get the user ID of the new user
+    // prepare select statement
+    if (!($stmt = $mysqli->prepare("SELECT userID FROM login WHERE username = ?"))) {
+      echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    // bind username and emailAddr params
+    if (!$stmt->bind_param("s", $_POST['username'])) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    // bind the results of the query to each field
+    $stmt->bind_result($userID);
+    $stmt->close();
 
     // create random 6 digit PIN for new user
     $newPIN = rand(100000,999999);
+
+    // get the existing account numbers
+    // prepare select statement
+    if (!($stmt = $mysqli->prepare("SELECT accountNum FROM accounts where accountNum = ?"))) {
+      echo "acct check Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
 
     do {
       // create account for new user
       $newAcctNum = rand(1000000000,9999999999);
 
-      // check to make sure the account number is not in use already
-      $acctCheck = $mysqli->query("SELECT * FROM accounts where accountNum = '$newAcctNum'");
-      $acctExists = $acctCheck->fetch_assoc();
+      // bind new account num param
+      if (!$stmt->bind_param("i", $newAcctNum)) {
+        echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+      }
 
-      // break if the account did not exist, else loop again
+      if (!$stmt->execute()) {
+        echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+      }
+
+      // bind the results of the query to each field
+      $stmt->bind_result($acctExists);
+      $stmt->close();
+
+      // break if the account number was not found in the DB, else loop again
       if (!isset($acctExists)) {
         break;
       }
     } while (true);
     echo "New Acct Number: " . $newAcctNum . "<br />";
     echo "New PIN: " . $newPIN;
-    $mysqli->query("INSERT INTO accounts (userID,accountNum,accountPIN) values ('$newUserID','$newAcctNum','$newPIN')");
+
+    // insert new account info into the DB
+    // prepare insert statement
+    if (!($stmt = $mysqli->prepare("INSERT INTO accounts (userID,accountNum,accountPIN) values (?, ?, ?)"))) {
+      echo "INSERT Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    // bind new account num param
+    if (!$stmt->bind_param("iii", $userID, $newAcctNum, $newPIN)) {
+      echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    if (!$stmt->execute()) {
+      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
   }
   else {
     echo "Vars not set :(";
   }
-
+  $mysqli->close();
 ?>
