abfb456796a1d9975ff829f93349bbe3943de656
Chaim Sanders <csanders@sparsa.org>
2016-02-20 19:29:47 -0500

    Update config.inc.php

--- BankOfSPARSA/includes/config.inc.php
+++ BankOfSPARSA/includes/config.inc.php
@@ -6,7 +6,7 @@ $root = realpath($_SERVER["DOCUMENT_ROOT"]) . $folder;
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'BankOfSPARSA');
 define('DB_USER','root');
-define('DB_PASSWORD','imxyubx');
+define('DB_PASSWORD','');
 define('BANK_API_ADDRESS','http://mycourses.tk');
 define('BANK_API_PORT','5000');
 define('LOCAL_ROUTING_NUMBER','01');  // routing number for this bank instance
