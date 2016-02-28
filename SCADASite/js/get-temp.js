b4df25822f5a784b03920c2f2e7ac8682f49964f
bjh7242 <bjh7242@rit.edu>
2016-02-27 23:14:17 -0500

    fixed changing div color with ajax requests for temp site

--- SCADASite/js/get-temp.js
+++ SCADASite/js/get-temp.js
@@ -1,14 +1,14 @@
 // Script to load temperature values from team temp records
-//setInterval(function() {loadDoc(url, getNumber)},3000);
+//setInterval(function() {loadDoc(url, getNumber, teamDivID)},3000);
 
 // loadDoc
-function loadDoc(url, teamID) {
+function loadDoc(url, teamID, teamDivID) {
   var xhttp;
   xhttp=new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       document.getElementById(teamID).innerHTML = xhttp.responseText + "&deg;F";
-      var div = document.getElementById("t5box");
+      var div = document.getElementById(teamDivID);
       if (xhttp.responseText > 70) {
         div.style.backgroundColor="red";
       }
@@ -21,6 +21,42 @@ function loadDoc(url, teamID) {
   xhttp.send();
 }
 
-//function getNumber(xhttp) {
-//  document.getElementById(teamID).innerHTML = xhttp.responseText + "&deg;F";
-//}
+
+// loadDoc
+function loadDoc1(url, teamID, teamDivID) {
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById(teamID).innerHTML = xhttp.responseText + "&deg;F";
+      var div = document.getElementById(teamDivID);
+      if (xhttp.responseText > 70) {
+        div.style.backgroundColor="red";
+      }
+      else {
+        div.style.backgroundColor="green";
+      }
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function loadDoc11(url, teamID, teamDivID) {
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById(teamID).innerHTML = xhttp.responseText + "&deg;F";
+      var div = document.getElementById(teamDivID);
+      if (xhttp.responseText > 70) {
+        div.style.backgroundColor="red";
+      }
+      else {
+        div.style.backgroundColor="green";
+      }
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
