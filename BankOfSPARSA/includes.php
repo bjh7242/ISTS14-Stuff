473653e4162b2cab71663f17e53219f7a0adf872
Chaim sanders <csanders@sparsa.org>
2016-02-27 23:25:13 -0500

    Added mobile data

--- BankOfSPARSA/includes.php
+++ BankOfSPARSA/includes.php
@@ -5,7 +5,13 @@
 include_once("includes/functions.inc.php"); 
 include_once("includes/config.inc.php");
 ?>
-<link href="<?php echo $domain?>/css/main.css" rel="stylesheet">
+<?php
+if(isMobile() == 0){
+	echo '<link href="'.$domain.'/css/main.css" rel="stylesheet">';
+}else{
+	echo '<link href="'.$domain.'/css/main-mobile.css" rel="stylesheet">';
+}
+?>
 </head>
 <body>
 <div id="bodyWrapper">
--- BankOfSPARSA/index.php
+++ BankOfSPARSA/index.php
@@ -2,8 +2,6 @@
 <?php $title="Login"; ?>
 <?php include("$root/header.php"); ?>
 
-
-
 	<div id="bigContent">
 		Welcome to the Bank of SPARSA. We are here to help you with all your banking needs. At Bank of SPARSA we offer exteremly competative lending and savings rates for all your banking needs. We want to be your bank of choice, and will do everything we can to facilitate that goal.  
 	<?php include("$root/twitinc.php"); ?>
