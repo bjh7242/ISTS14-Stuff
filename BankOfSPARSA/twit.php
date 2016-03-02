b3169deb18e1a75464f82eecae9621db3c927e1e
Chaim sanders <csanders@sparsa.org>
2016-03-02 14:09:31 -0500

    Fixed twitter date mixup

--- BankOfSPARSA/twit.php
+++ BankOfSPARSA/twit.php
@@ -64,7 +64,11 @@ foreach($a as $node){
     $out2 = strpos($t[2],'h');
     $out3 = strpos($t[2],'d');
     $out1 = $out1 + $out2 + $out3;
-    $t[2] =  substr($t[2],0,$out1+1);
+    if(strlen($out1) == 1){
+        $t[2] = $t[2];
+    }else{
+        $t[2] =  substr($t[2],0,$out1+1);
+    }
     echo '<div id="tweet">';
     echo '  <div id="image"><img width=28px height=28px style="float:left;" src="images/globe.png"></div>';
     echo '  <div style="float:left;" id="tweetText">';
