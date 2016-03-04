17c8faea56dc7384272e0fd5fc37c8c8b181839a
bjh7242 <bjh7242@rit.edu>
2016-03-04 00:01:42 -0500

    added authentication verification for admin panel

--- BankOfSPARSA/admin/panel.php
+++ BankOfSPARSA/admin/panel.php
@@ -3,6 +3,14 @@
 	include("$root/admin/content.php");
 	$title="Login";
 	include("$root/header.php");
+	// if not logged in as admin or no valid session exists, don't load the page
+	if (isset($_SESSION['role']) and $_SESSION['role'] != 'admin' or !isset($_SESSION['role'])) {
+		echo '<div id="bigContent">';
+		echo "Unauthorized.";
+		echo '</div>';
+		include("$root/footer.php");
+		die();
+	}
 ?>
 <div id="bigContent">
   <?php
