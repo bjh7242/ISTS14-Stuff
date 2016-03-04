326fe2a0b5735fe0b93baebba0a0c89c840ea87f
bjh7242 <bjh7242@rit.edu>
2016-03-04 15:16:51 -0500

    changes fed password successfully with error handling.

--- BankOfSPARSA/admin/change_fed_password.php
+++ BankOfSPARSA/admin/change_fed_password.php
@@ -1,13 +1,16 @@
 <?php
-  include_once('content.php');
-  include_once('../includes.php');
+  include("../includes.php");
+  include("$root/admin/content.php");
+  $title="Change Fed Password";
+  include("$root/header.php");
 
   function change_password($newPassword) {
+    global $root;
     $sessionID = get_session();
-    echo "SESSION ID IS: " . htmlspecialchars($sessionID) . "\n";
+    //echo "SESSION ID IS: " . htmlspecialchars($sessionID) . "\n";
     if (!isset($sessionID)) {
       echo "sessionID is not set?";
-      echo htmlspecialchars($sessionID);
+      //echo htmlspecialchars($sessionID);
       die();
     }
     else {
@@ -16,22 +19,27 @@
       $args  = array(
         'accountNum' => API_ACCOUNT_NUM,
         'session' => $sessionID,
-        'newPassword' => $newPassword
+        'newPassword' => $newPassword,
+        'password' => $_POST['current_password']
       );
       $result = build_api_request('/changePassword',$args);
       // error handling for changing the password
-      if (!isset($result->Status)) {
-        echo $result;
+      $json_result = json_decode($result);
+      //echo "json_result-Status = " . $json_result->Status;
+      if (!isset($json_result->Status)) {
+        echo '<div id="bigContent">';
+        echo "Invalid request.";
+        echo '</div>';
+        include("$root/footer.php");
         die();
       }
-      $json_result = json_decode($result);
-      // if there is an error, $json_result->Status will return 'False'
-      if (!isset($json_result->Status)) {
-        echo "password change worked.";
-        echo $json_result;
-      }
       else {
-        echo $json_result->Status;
+        echo '<div id="bigContent">';
+        echo $json_result->Status . '<br />';
+        echo "Please update your config.inc.php file to reflect your changed password in order to prevent errors when accessing the fed API.";
+        echo '</div>';
+        include("$root/footer.php");
+        die();
       }
     }
 
@@ -40,7 +48,11 @@
   // verify that the new password matches both entries
   function verify_matching_password() {
     if ($_POST['new_password'] !== $_POST['new_password2']) {
-      echo "ERROR: passwords do not match.";
+      global $root;
+      echo '<div id="bigContent">';
+      echo "Passwords do not match.";
+      echo '</div>';
+      include("$root/footer.php");
       die();
     }
   }
